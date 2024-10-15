1a9add43800a2e2941187b02eeb7952c67db03d6
SimorqDev <morteza_hadavi@hotmail.com>
2024-10-15 14:25:28 +0300

    V 0.1

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+#!
+"""
+
+TOKEN = '' #API TOKEN FROM BotFather
+MySQL = ['localhost', 'user', 'password', 'Database_name'] #connection to mariadb/mysql
+ADMIN_ID = '' #Type SuperUser ID 
+PROXY_URL = '' #additional
