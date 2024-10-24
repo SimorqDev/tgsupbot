20fcd76b96a63ed491934bc8ebfdaabade2614ac
SimorqDev <morteza_hadavi@hotmail.com>
2024-10-24 11:03:19 +0300

    v 0.1

---
+++ text.py
@@ -0,0 +1,39 @@
+"""
+Text for bot.py
+"""
+
+
+
+
+
+
+"""
+TEXT for markup.py
+"""
+
+
+value_m1 = "✏️ Write a request"
+value_m2 = "✉️ My requests"
+value_a1 = "❗️Waiting for a response from support"
+value_a2 = "⏳ Waiting for a response from the user"
+value_a3 = "✅ Completed requests"
+
+value_ad1 = "✅ Add support agent"
+value_ad2 = "🧑‍💻 Support Agents"
+value_ad3 = "🔑 temp passwords"
+value_ad4 = "🎲 Generate one-time passwords"
+value_ad5 = "⛔️ Turn off bot"
+
+"""
+General Text
+"""
+
+back = "Back"
+cancel = "Cancel"
+yes = "Yes"
+no = "No"
+confirm = "Confirm ✅ "
+complete = "Complete the request ✅ "
+showfile = "🗂 Show Files"
+addmsg = "✏️ Add Message"
+
